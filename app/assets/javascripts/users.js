bcec50347fc9c366d5db25a298c54513a9747b00
kmasanori <kmasanori0221@gmail.com>
2020-01-07 13:07:18 -0800

    make_codes_beautiful

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -17,6 +17,7 @@ $(function() {
     `;
     $("#user-search-result").append(html);
   }
+
   function addDeleteUser(name, id) {
     let html = `
     <div class="chat-group-user clearfix" id="${id}">
@@ -25,10 +26,12 @@ $(function() {
     </div>`;
     $(".js-add-user").append(html);
   }
+
   function addMember(userId) {
     let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
     $(`#${userId}`).append(html);
   }
+
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     $.ajax({
@@ -54,6 +57,7 @@ $(function() {
         alert("通信エラーです。ユーザーが表示できません。");
       });
   });
+
   $(document).on("click", ".chat-group-user__btn--add", function() {
     console.log
     const userName = $(this).attr("data-user-name");
@@ -64,6 +68,7 @@ $(function() {
     addDeleteUser(userName, userId);
     addMember(userId);
   });
+  
   $(document).on("click", ".chat-group-user__btn--remove", function() {
     $(this)
       .parent()
